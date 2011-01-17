1e2279145d09183b4cef0d60b47658b7c4ebd21d
Edijs Petersons <edijs@Edijs-Petersonss-MacBook-Pro.local>
2011-01-18 00:05:26 +0100

    changed admin password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
           user_name == 'admin' && password == 'password'
-          || user_name == 'admin' && password == 'admin'
+          || user_name == 'admin' && password == 'admin1'
     end
   end
 end
