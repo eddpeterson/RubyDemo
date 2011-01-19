0be6ad732644ced3df84a385976b03de76d4c826
Edijs Petersons <eddpeterson@gmail.com>
2011-01-19 01:12:52 +0100

    added change in branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |user_name, password|
           user_name == 'admin' && password == 'password'
           || user_name == 'admin' && password == 'admin'
+          || user_name == 'admin1' && password == 'password1'
     end
   end
 end
